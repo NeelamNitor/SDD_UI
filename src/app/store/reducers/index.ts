6b51111231a83b30d8ae153ff2de93e495b5eb07
neelam.varhadi <neelam.varhadi@nitorinfotech.com>
2026-06-08 21:09:22 +0530

    Integration changes

--- src/app/store/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import { ActionReducerMap } from '@ngrx/store';
-
-export interface AppState {
-  // Define your state interfaces here
-}
-
-export const rootReducer: ActionReducerMap<AppState> = {
-  // Add your reducers here
-};
